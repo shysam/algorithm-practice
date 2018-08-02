49c123abb6068671ae18a873a73bd5e02ebd97e5
shysam <samtos@163.com>
2018-08-02 17:26:03 +0800

    新增归并排序

---
+++ src/sort/basis/merge/Merge.java
@@ -0,0 +1,52 @@
+package sort.basis.merge;
+
+import sort.basis.Operation;
+
+/**
+ * 归并排序 - 稳定
+ */
+public class Merge extends Operation {
+
+    public static void main(String[] args) {
+        int[] a = new int[]{23, 42, 12, 65, 7, 34, 98, 5, 34, 23, 657, 78, 34, 56};
+        sort(a);
+        show(a);
+    }
+
+    public static void sort(int[] a) {
+        int[] aux = new int[a.length];
+        sort(a, aux, 0, a.length - 1);
+    }
+
+    private static void sort(int[] a, int[] aux, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+
+    private static void merge(int[] a, int[] aux, int lo, int mid, int hi) {
+        // copy to aux[]
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        // merge back to a[]
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+
+}
