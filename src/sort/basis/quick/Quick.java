6f4f6c5cd7f9c8231373c197953ba3d346fb5f30
shysam <samtos@163.com>
2018-08-02 17:23:44 +0800

    新增快速排序

---
+++ src/sort/basis/quick/Quick.java
@@ -0,0 +1,66 @@
+package sort.basis.quick;
+
+import sort.basis.Operation;
+
+/**
+ * 快速排序 - 不稳定
+ */
+public class Quick extends Operation {
+
+    public static void main(String[] args) {
+        int[] a = new int[]{23, 42, 12, 65, 7, 34, 98, 5, 34, 23, 657, 78, 34, 56};
+        sort(a);
+        show(a);
+    }
+
+    public static void sort(int[] a) {
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(int[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    /**
+     * 找切分点，交换
+     *
+     * @param a
+     * @param lo
+     * @param hi
+     * @return
+     */
+    private static int partition(int[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        int v = a[lo];
+        while (true) {
+
+            // find item on lo to swap
+            while (less(a[++i], v)) {
+                if (i == hi) break;
+            }
+
+            // find item on hi to swap
+            while (less(v, a[--j])) {
+                if (j == lo) break;      // redundant since a[lo] acts as sentinel
+            }
+
+            // check if pointers cross
+            if (i >= j) break;
+
+            exch(a, i, j);
+        }
+
+        // put partitioning item v at a[j]
+        exch(a, lo, j);
+
+        // now, a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
+        return j;
+    }
+
+}
